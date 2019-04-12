33baa13a2a438989409515d025b4c9a32ce85c27
Maciek Szul <maciek.pwns@gmail.com>
2019-04-12 10:48:41 +0100

    ADD: watershed + scalp surf

--- pipeline.py
+++ pipeline.py
@@ -25,9 +25,10 @@ parser.add_argument(
 
 args = parser.parse_args()
 params = vars(args)
-json_file = params["f"]
+json_file = str(params["f"][0])
 subj_index = params["n"]
 
+print(json_file)
 # read the pipeline params
 with open(json_file) as pipeline_file:
     pipeline_params = json.load(pipeline_file)
@@ -60,47 +61,83 @@ files.make_folder(t1_subj_dir)
 
 def dcm2nii_func(t1_subj_dir, raw_subj_dir):
     dcm2nii = "/cubric/software/mricron/dcm2nii"
-
-    sp.call([
-        dcm2nii,
-        "-4", "y", 
-        "-a", "n", 
-        "-c", "n", 
-        "-d", "n", 
-        "-e", "n", 
-        "-f", "n", 
-        "-g", "y", 
-        "-i", "n", 
-        "-o", t1_subj_dir, 
-        "-p", "n", 
-        "-r", "y", 
-        "-v", "y", 
-        "-x", "y", 
-        raw_subj_dir
-    ])
+    try:
+        t1_file = files.get_files(
+            t1_subj_dir,
+            "co",
+            "nii.gz"
+        )[2][0]
+        print(raw_subj, op.exists(t1_file), "T1 image exists")
+    except:
+        sp.call([
+            dcm2nii,
+            "-4", "y", 
+            "-a", "n", 
+            "-c", "n", 
+            "-d", "n", 
+            "-e", "n", 
+            "-f", "n", 
+            "-g", "y", 
+            "-i", "n", 
+            "-o", t1_subj_dir, 
+            "-p", "n", 
+            "-r", "y", 
+            "-v", "y", 
+            "-x", "y", 
+            raw_subj_dir
+        ])
 
 if pipeline_params["t1_conversion"]:
     dcm2nii_func(t1_subj_dir, raw_subj_dir)
 
-t1_file = files.get_files(
-    t1_subj_dir,
-    "co",
-    "nii.gz"
-)[2][0]
+def recon_all_func(fs_path, raw_subj):
+    try:
+        t1_file = files.get_files(
+            t1_subj_dir,
+            "co",
+            "nii.gz"
+        )[2][0]
 
-def recon_all_func(t1_file, fs_path, raw_subj):
-    sp.call([
-        "recon-all",
-        "-i", t1_file,
-        "-subjid", raw_subj,
-        "-sd", fs_path,
-        "-all"
-    ])
+        sp.call([
+            "recon-all",
+            "-i", t1_file,
+            "-subjid", raw_subj,
+            "-sd", fs_path,
+            "-all"
+        ])
+    except:
+        print(raw_subj, "No T1 file for this participant")
+    
 
 fs_subj_dir = op.join(
     fs_path,
     raw_subj
 )
 
-if not op.exists(fs_subj_dir) and pipeline_params["recon_all"]:
-    recon_all_func(t1_file, fs_path, raw_subj)
+if pipeline_params["recon_all"] and not op.exists(fs_subj_dir):
+    recon_all_func(fs_path, raw_subj)
+
+def bem_watershed_func(fs_path, raw_subj, overwrite=False):
+    sp.call([
+        "mne",
+        "watershed_bem",
+        "-s", raw_subj,
+        "-d", fs_path,
+        "-o", overwrite
+    ])
+
+if pipeline_params["bem_watershed"] and op.exists(fs_subj_dir):
+    bem_watershed_func(fs_path, raw_subj)
+
+def make_scalp_surface_func(fs_path, raw_subj, overwrite=False):
+    sp.call([
+        "mne",
+        "make_scalp_surfaces",
+        "-s", raw_subj,
+        "-d", fs_path,
+        "-f", True,
+        "-o", overwrite
+    ])
+
+if pipeline_params["make_scalp_surface"]:
+    make_scalp_surface_func(fs_path, raw_subj)
